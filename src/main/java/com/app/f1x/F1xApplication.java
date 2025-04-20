5b86932db01e867a10ddeee45cadc6a2dca2fce3
tqcss <134121905+tqcss@users.noreply.github.com>
2025-04-20 13:54:08 +0800

    initial commit

---
+++ src/main/java/com/app/f1x/F1xApplication.java
@@ -0,0 +1,13 @@
+package com.app.f1x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1xApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1xApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/f1x/F1xApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.f1x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1xApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
