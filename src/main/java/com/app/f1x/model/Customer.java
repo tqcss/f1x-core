f2b6e607a02fafa4cb3722a426b52133246b4f1a
tqcss <134121905+tqcss@users.noreply.github.com>
2025-04-23 04:30:07 +0800

    remove unused import

--- src/main/java/com/app/f1x/model/Customer.java
+++ src/main/java/com/app/f1x/model/Customer.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import java.util.Optional;
 
 @Entity
 public class Customer {
