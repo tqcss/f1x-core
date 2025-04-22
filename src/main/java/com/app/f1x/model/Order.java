b0142575f3a3017a1d2c514f627992fb80344a77
tqcss <134121905+tqcss@users.noreply.github.com>
2025-04-23 05:05:31 +0800

    replace stars with single imports

--- src/main/java/com/app/f1x/model/Order.java
+++ src/main/java/com/app/f1x/model/Order.java
@@ -1,7 +1,16 @@
 package com.app.f1x.model;
 
 import com.app.f1x.model.enums.OrderStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 import java.time.LocalDateTime;
 import java.util.List;
